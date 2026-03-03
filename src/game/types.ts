26cc6c0b3cfe796187af82e53b221b7f92bae762
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-03-03 10:25:24 +0100

    Create types.ts

---
+++ src/game/types.ts
@@ -0,0 +1,51 @@
+export type Settings = {
+  sound: boolean;
+  haptics: boolean;
+  retroOverlay: boolean;
+};
+
+export type HudState = {
+  score: number;
+  lives: number;
+  level: number;
+  paused: boolean;
+  gameOver: boolean;
+};
+
+export type Vec2 = { x: number; y: number };
+
+export type Paddle = {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+};
+
+export type Ball = {
+  p: Vec2;
+  v: Vec2;
+  r: number;
+  stuckToPaddle: boolean;
+};
+
+export type Brick = {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+  hp: number; // 1 for MVP
+  alive: boolean;
+};
+
+export type World = {
+  width: number;
+  height: number;
+  paddle: Paddle;
+  ball: Ball;
+  bricks: Brick[];
+  score: number;
+  lives: number;
+  level: number;
+  paused: boolean;
+  gameOver: boolean;
+};
